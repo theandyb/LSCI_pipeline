826827f8518bcdb23201fc0751be46bb0f6d3f84
Andy Beck <beckandy@umich.edu>
2023-12-18 15:16:41 -0500

    correct vcf filenames

--- src/extract_singletons_AMR.sh
+++ src/extract_singletons_AMR.sh
@@ -15,6 +15,6 @@
 
 OUT_DIR="../output/singletons/AMR/"
 
-bcftools view -i "%FILTER=='PASS'" -v snps ../data/reference/20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chr${SLURM_ARRAY_TASK_ID}.recalibrated_variants.annotated.vcf.gz |\
+bcftools view -i "%FILTER=='PASS'" -v snps ../data/1kgp/20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chr${SLURM_ARRAY_TASK_ID}.recalibrated_variants.annotated.vcf.gz |\
 bcftools view -i "(AC_AMR_unrel == 1 | (AC_Hom_AMR_unrel == 2 & AC_Het_AMR_unrel ==0)) & AC_EUR_unrel == 0 & AC_AFR_unrel == 0 & AC_EAS_unrel == 0 & AC_SAS_unrel == 0" |\
 bcftools query -f "%CHROM\t%POS\t%REF\t%ALT\n" > $OUT_DIR/chr${SLURM_ARRAY_TASK_ID}.txt
