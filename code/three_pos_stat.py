6b0f6f89c9507c20103acd4383f02de87a6da469
Andy Beck <beckandy@umich.edu>
2025-03-04 15:33:05 -0500

    fix nucleotide counting

--- code/three_pos_stat.py
+++ code/three_pos_stat.py
@@ -30,7 +30,7 @@ def c_pos(subtype, chromosome, pop):
   f_name = input_dir + subtype + "_" + str(chromosome) + ".txt"
   pos_list = pd.read_csv(f_name, header=0, names = ['pos'], usecols=['pos']).squeeze("columns")
   return pos_list
-  
+
 def s_pos(subtype, chromosome, pop = "ALL"):
   """Get the positions for singletons for a given subtype"""
   input_dir = "output/singletons/{}/pos_files/".format(pop)
@@ -38,11 +38,24 @@ def s_pos(subtype, chromosome, pop = "ALL"):
   pos_list = pd.read_csv(f_name, header=None, names = ['pos'], usecols=['pos']).squeeze("columns")
   return pos_list
 
-def complement(nucleotide):
-  complements = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
-  if not nucleotide in complements.keys():
-    return nucleotide
-  return complements[nucleotide]
+def generate_nucleotide_motifs(n):
+  if n <= 0:
+    return []
+  nucleotides = ['A', 'C', 'G', 'T']
+  motifs = ['']
+  for _ in range(n):
+    new_motifs = []
+    for motif in motifs:
+      for nucleotide in nucleotides:
+        new_motifs.append(motif + nucleotide)
+    motifs = new_motifs
+  return {item: 0 for item in motifs}
+
+def reverse_complement(dna_sequence):
+  complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C', 'N':'N', 'a':'t', 't':'a', 'c':'g', 'g':'c', 'n':'n'} #added lowercase and N support.
+  reverse_seq = dna_sequence[::-1]  # Reverse the sequence
+  reverse_complement_seq = "".join(complement.get(base, base) for base in reverse_seq) #get complement, or return original base if not found.
+  return reverse_complement_seq
 
 def get_seq_db(chromosome, db_file="data/reference/ref_db.db"):
   db_file_ro = "file:{}?mode=ro".format(db_file)
@@ -55,45 +68,27 @@ def get_seq_db(chromosome, db_file="data/reference/ref_db.db"):
   return(result[0])
 
 @ray.remote
-def get_count_table_control(chromosome, subtype, p, q, r, pop = "ALL"):
+def get_count_table_control(chromosome, subtype, rel_vals = [-1,0,1], pop = "ALL"):
   control_pos = c_pos(subtype, chromosome, pop)
   rev_control_pos = c_pos(subtype + "_rev", chromosome, pop)
-  seqstr = get_seq_db(chromosome)
-  results = {"AAA":0, "AAC":0, "AAG":0, "AAT":0, 
-    "ACA":0, "ACC":0, "ACG":0, "ACT":0,
-    "AGA":0, "AGC":0, "AGG":0, "AGT":0,
-    "ATA":0, "ATC":0, "ATG":0, "ATT":0,
-    "CAA":0, "CAC":0, "CAG":0, "CAT":0, 
-    "CCA":0, "CCC":0, "CCG":0, "CCT":0,
-    "CGA":0, "CGC":0, "CGG":0, "CGT":0,
-    "CTA":0, "CTC":0, "CTG":0, "CTT":0,
-    "GAA":0, "GAC":0, "GAG":0, "GAT":0, 
-    "GCA":0, "GCC":0, "GCG":0, "GCT":0,
-    "GGA":0, "GGC":0, "GGG":0, "GGT":0,
-    "GTA":0, "GTC":0, "GTG":0, "GTT":0,
-    "TAA":0, "TAC":0, "TAG":0, "TAT":0, 
-    "TCA":0, "TCC":0, "TCG":0, "TCT":0,
-    "TGA":0, "TGC":0, "TGG":0, "TGT":0,
-    "TTA":0, "TTC":0, "TTG":0, "TTT":0
-    }
+  seq = get_seq_db(chromosome)
+  results = generate_nucleotide_motifs(3)
   for index, value in control_pos.items():
-    ix1 = value - 1 + p
-    ix2 = value - 1 + q
-    ix3 = value - 1 + r
-    nuc = "{}{}{}".format(seqstr[ix1],seqstr[ix2],seqstr[ix3])
+    ix_vals = [(value - 1) + item for item in rel_vals]
+    nuc = "".join([seq[item] for item in ix_vals])
     if nuc in results.keys():
       results[nuc] += 1
-  for index, value in rev_control_pos.items():
-    ix1 = value - 1 + (p * -1)
-    ix2 = value - 1 + (q * -1)
-    ix3 = value - 1 + (r * -1)
-    nuc = "{}{}{}".format(complement(seqstr[ix1]),complement(seqstr[ix2]),complement(seqstr[ix3]))
+  for index, value in rev_control_pos.items(): # reverse complement: need to take inverse (x-1) of each offset, sort, get nucleotide motif, and then reverse-complement
+    ix_vals = [(value - 1) + (item*-1)  for item in rel_vals]
+    ix_vals.sort()
+    nuc = "".join([seq[item] for item in ix_vals])
+    nuc = reverse_complement(nuc)
     if nuc in results.keys():
       results[nuc] += 1
-  return(results)
+  return {key: value for key, value in results.items() if value != 0}
 
 @ray.remote
-def get_count_table_singletons(chromosome, subtype, p, q, r, pop = "ALL"):
+def get_count_table_singletons(chromosome, subtype, rel_vals = [-1, 0, 1], pop = "ALL"):
   """
   Get the count table for singletons at a given relative position for a subtype.
   Note: this version has been made so as to be called in parallel via ray, but
@@ -103,56 +98,38 @@ def get_count_table_singletons(chromosome, subtype, p, q, r, pop = "ALL"):
   """
   singleton_pos = s_pos(subtype, chromosome, pop)
   rev_singleton_pos = s_pos(subtype + "_rev", chromosome, pop)
-  seqstr = get_seq_db(chromosome)
-  results = {"AAA":0, "AAC":0, "AAG":0, "AAT":0, 
-    "ACA":0, "ACC":0, "ACG":0, "ACT":0,
-    "AGA":0, "AGC":0, "AGG":0, "AGT":0,
-    "ATA":0, "ATC":0, "ATG":0, "ATT":0,
-    "CAA":0, "CAC":0, "CAG":0, "CAT":0, 
-    "CCA":0, "CCC":0, "CCG":0, "CCT":0,
-    "CGA":0, "CGC":0, "CGG":0, "CGT":0,
-    "CTA":0, "CTC":0, "CTG":0, "CTT":0,
-    "GAA":0, "GAC":0, "GAG":0, "GAT":0, 
-    "GCA":0, "GCC":0, "GCG":0, "GCT":0,
-    "GGA":0, "GGC":0, "GGG":0, "GGT":0,
-    "GTA":0, "GTC":0, "GTG":0, "GTT":0,
-    "TAA":0, "TAC":0, "TAG":0, "TAT":0, 
-    "TCA":0, "TCC":0, "TCG":0, "TCT":0,
-    "TGA":0, "TGC":0, "TGG":0, "TGT":0,
-    "TTA":0, "TTC":0, "TTG":0, "TTT":0
-    }
+  seq = get_seq_db(chromosome)
+  results = generate_nucleotide_motifs(3)
   for index, value in singleton_pos.items():
-    ix1 = value - 1 + p
-    ix2 = value - 1 + q
-    ix3 = value - 1 + r
-    nuc = "{}{}{}".format(seqstr[ix1],seqstr[ix2],seqstr[ix3])
+    ix_vals = [(value - 1) + item for item in rel_vals]
+    nuc = "".join([seq[item] for item in ix_vals])
     if nuc in results.keys():
       results[nuc] += 1
   for index, value in rev_singleton_pos.items():
-    ix1 = value - 1 + (p * -1)
-    ix2 = value - 1 + (q * -1)
-    ix3 = value - 1 + (r * -1)
-    nuc = "{}{}{}".format(complement(seqstr[ix1]),complement(seqstr[ix2]), complement(seqstr[ix3]))
+    ix_vals = [(value - 1) + (item*-1)  for item in rel_vals]
+    ix_vals.sort()
+    nuc = "".join([seq[item] for item in ix_vals])
+    nuc = reverse_complement(nuc)
     if nuc in results.keys():
       results[nuc] += 1
   return(results)
 
-def get_count_all(subtype, p, q, r, status = "singleton", pop = "ALL"):
+def get_count_all(subtype, rel_vals = [-1,0,1], status = "singleton", pop = "ALL"):
   """
   Get the singleton and control counts for a given relative position
   across all 22 autosomes. This version is parallelized via ray
   """
   if status == "singleton":
-    futures = [get_count_table_singletons.remote(i, subtype, p, q, r, pop) for i in range(1,23)]
+    futures = [get_count_table_singletons.remote(i, subtype, rel_vals = rel_vals, pop = pop) for i in range(1,23)]
     results = pd.DataFrame.from_dict(ray.get(futures)).sum(axis=0)
   else:
-    futures = [get_count_table_control.remote(i, subtype, p, q, r, pop) for i in range(1,23)]
+    futures = [get_count_table_control.remote(i, subtype, rel_vals = rel_vals, pop = pop) for i in range(1,23)]
     results = pd.DataFrame.from_dict(ray.get(futures)).sum(axis=0)
   return(results)
 
-def fit_model_all(subtype, p, q, r, pop = "ALL"):
-  df_s = get_count_all(subtype, p, q, r, pop = pop)
-  df_c = get_count_all(subtype, p, q, r, status = "control", pop = pop)
+def fit_model_all(subtype, rel_vals, pop = "ALL"):
+  df_s = get_count_all(subtype, rel_vals = rel_vals, pop = pop)
+  df_c = get_count_all(subtype, rel_vals = rel_vals, status = "control", pop = pop)
   df_s = df_s.rename_axis("motif").reset_index()
   df_c = df_c.rename_axis("motif").reset_index()
   df_s.columns = ["motif", "singletons"]
@@ -170,7 +147,7 @@ def fit_model_all(subtype, p, q, r, pop = "ALL"):
   mod = smf.glm("n ~ status + p1 + p2 + p1*p2*p3 + p1*p2*status + p2*p3*status + p1*p3*status", df, family = sm.families.Poisson()).fit()
   n_s = sum(df_s.singletons)
   n_c = sum(df_c.controls)
-  return {"dev":mod.deviance, "singletons":n_s, "controls":n_c, "rp1":p, "rp2":q, "rp3": r}
+  return {"dev":mod.deviance, "singletons":n_s, "controls":n_c, "rp1":rel_vals[0], "rp2":rel_vals[1], "rp3": rel_vals[2]}
 
 ray.init(num_cpus=22)
 results = []
@@ -181,20 +158,20 @@ for p1 in range(-10,9):
         continue
     if subtype.startswith("cpg") and p1 == 1:
         continue
-    print("p1: {}".format(p1))
+    print("p1: {}".format(p1), flush=True)
     for p2 in range((p1+1),10):
         if p2 == 0:
             continue
         if subtype.startswith("cpg") and p2 == 1:
             continue
-        print("p2: {}".format(p2))
+        print("p2: {}".format(p2), flush=True)
         for p3 in range((p2+1),11):
             if p3 == 0:
                 continue
             if subtype.startswith("cpg") and p3 == 1:
                 continue
-            print("p3: {}".format(p3))
-            results.append(fit_model_all(subtype, p1, p2, p3, pop = pop))
+            print("p3: {}".format(p3), flush=True)
+            results.append(fit_model_all(subtype, [p1, p2, p3], pop = pop))
 
 ray.shutdown()
 final = pd.DataFrame.from_dict(results)
